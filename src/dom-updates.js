ae44bf49c2fe87b0cb72cc761cec62b227dd1d5a
Taras Tarlov <ttarlov@gmail.com>
2020-02-20 08:35:29 -0700

    :crystal-ball: Add addCardsToDom method. Renders cards on the DOM.

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -10,19 +10,19 @@ let domUpdates = {
   },
 
 
-  // addToDom: (recipeInfo, shortRecipeName) => {
-  //     $('main').append(`<div class="recipe-card" id=${recipeInfo.id}>
-  //       <h3 maxlength="40">${shortRecipeName}</h3>
-  //       <div class="card-photo-container">
-  //         <img src=${recipeInfo.image} class="card-photo-preview" alt="${recipeInfo.name} recipe" title="${recipeInfo.name} recipe">
-  //         <div class="text">
-  //           <div>Click for Instructions</div>
-  //         </div>
-  //       </div>
-  //       <h4>${recipeInfo.tags[0]}</h4>
-  //       <img src="../images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
-  //     </div>`);
-      // };
+  addCardsToDom(recipeInfo, shortRecipeName) {
+      $('main').append(`<div class="recipe-card" id=${recipeInfo.id}>
+        <h3 maxlength="40">${shortRecipeName}</h3>
+        <div class="card-photo-container">
+          <img src=${recipeInfo.image} class="card-photo-preview" alt="${recipeInfo.name} recipe" title="${recipeInfo.name} recipe">
+          <div class="text">
+            <div>Click for Instructions</div>
+          </div>
+        </div>
+        <h4>${recipeInfo.tags[0]}</h4>
+        <img src="../images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
+      </div>`);
+    }
 
 
 
