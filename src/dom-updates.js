d051b70ecc12718410f598b78e289e14e1a73e21
Taras Tarlov <ttarlov@gmail.com>
2020-02-20 13:51:19 -0700

    :crystal_ball: add listTags and capitalize methods.

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -22,11 +22,29 @@ let domUpdates = {
         <h4>${recipeInfo.tags[0]}</h4>
         <img src="../images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
       </div>`);
-    }
+    },
+
+  listTags(allTags) {
+
+    allTags.forEach(tag => {
+      $(".tag-list").append(`<li><input type="checkbox" class="checked-tag" id="${tag}">
+        <label for="${tag}">${this.capitalize(tag)}</label></li>`);
+      })
+
+  },
+
+  capitalize(words) {
+    return words.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  }
 
 
 
 
-};
+
+
+
+}
 
 export default domUpdates;
