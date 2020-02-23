8fa631534a44cad1c360623b52ee7e2ce488172a
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-23 15:44:35 -0700

    Add test for method to determine if a user pantry has the ingredients to make a recipe

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -11,133 +11,133 @@ describe('Pantry', function() {
   beforeEach(function() {
     pantryData = [
                 {
-                  "ingredient": 11477,
+                  "id": 11477,
                   "amount": 4,
                   "name": "zucchini squash",
                   "estimatedCostInCents": 742
                 },
                 {
-                  "ingredient": 11297,
+                  "id": 11297,
                   "amount": 4,
                   "name": "flat leaf parsley leaves",
                   "estimatedCostInCents": 1030
                 },
                 {
-                  "ingredient": 1082047,
+                  "id": 1082047,
                   "amount": 10,
                   "name": "kosher salt",
                   "estimatedCostInCents": 972
                 },
                 {
-                  "ingredient": 20081,
+                  "id": 20081,
                   "amount": 5,
                   "name": "wheat flour",
                   "estimatedCostInCents": 142
                 },
                 {
-                  "ingredient": 11215,
+                  "id": 11215,
                   "amount": 5,
                   "name": "wheat flour",
                   "estimatedCostInCents": 142
                 },
                 {
-                  "ingredient": 2047,
+                  "id": 2047,
                   "amount": 6,
                   "name": "salt",
                   "estimatedCostInCents": 280
                 },
                 {
-                  "ingredient": 1123,
+                  "id": 1123,
                   "amount": 8,
                   "name": "eggs",
                   "estimatedCostInCents": 472
                 },
                 {
-                  "ingredient": 11282,
+                  "id": 11282,
                   "amount": 4,
                   "name": "onions",
                   "estimatedCostInCents": 439
                 },
                 {
-                  "ingredient": 6172,
+                  "id": 6172,
                   "amount": 2,
                   "name": "chicken stock",
                   "estimatedCostInCents": 454
                 },
                 {
-                  "ingredient": 2044,
+                  "id": 2044,
                   "amount": 2,
                   "name": "basil",
                   "estimatedCostInCents": 203
                 },
                 {
-                  "ingredient": 2050,
+                  "id": 2050,
                   "amount": 4,
                   "name": "vanilla",
                   "estimatedCostInCents": 926
                 },
                 {
-                  "ingredient": 1032009,
+                  "id": 1032009,
                   "amount": 3,
                   "name": "dried red chili",
                   "estimatedCostInCents": 1015
                 },
                 {
-                  "ingredient": 5114,
+                  "id": 5114,
                   "amount": 3,
                   "name": "roasted chicken",
                   "estimatedCostInCents": 708
                 },
                 {
-                  "ingredient": 1017,
+                  "id": 1017,
                   "amount": 2,
                   "name": "cream cheese",
                   "estimatedCostInCents": 955
                 },
                 {
-                  "ingredient": 19334,
+                  "id": 19334,
                   "amount": 7,
                   "name": "bicarbonate of soda",
                   "estimatedCostInCents": 582
                 },
                 {
-                  "ingredient": 1001,
+                  "id": 1001,
                   "amount": 6,
                   "name": "butter",
                   "estimatedCostInCents": 618
                 },
                 {
-                  "ingredient": 1145,
+                  "id": 1145,
                   "amount": 2,
                   "name": "canned chipotle chilies in adobo",
                   "estimatedCostInCents": 299
                 },
                 {
-                  "ingredient": 10019903,
+                  "id": 10019903,
                   "amount": 2,
                   "name": "buttermilk",
                   "estimatedCostInCents": 773
                 },
                 {
-                  "ingredient": 9152,
+                  "id": 9152,
                   "amount": 4,
                   "name": "lemon juice",
                   "estimatedCostInCents": 274
                 },
                 {
-                  "ingredient": 10611282,
+                  "id": 10611282,
                   "amount": 3,
                   "name": "white onions",
                   "estimatedCostInCents": 449
                 },
                 {
-                  "ingredient": 93607,
+                  "id": 93607,
                   "amount": 2,
                   "name": "almondmilk",
                   "estimatedCostInCents": 787
                 },
                 {
-                  "ingredient": 14106,
+                  "id": 14106,
                   "amount": 4,
                   "name": "white wine",
                   "estimatedCostInCents": 675
@@ -223,7 +223,7 @@ describe('Pantry', function() {
                       "amount": 0.5,
                       "unit": "tsp"
                   }
-              }
+              }]
             }
     pantry = new Pantry(pantryData);
   })
@@ -237,6 +237,6 @@ describe('Pantry', function() {
   });
 
   it('should determine whether my pantry has enough ingredients to cook a given meal', function() {
-
+    expect(pantry.hasEnoughIngredientsForRecipe(recipeData)).to.eq(true);
   })
 });
