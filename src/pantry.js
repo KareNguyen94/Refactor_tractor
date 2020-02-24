82ffe10593204c75dfe6dc4bc9fd8b4b7ca63570
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-24 14:35:28 -0700

    Refactor ingredientsNeededForARecipe method

--- src/pantry.js
+++ src/pantry.js
@@ -3,37 +3,47 @@ class Pantry {
     this.ingredients = pantryIngredients;
   }
 
+  findIngredient(ingredients, searchIngredient) {
+    return ingredients.find(ingredient => {
+      return ingredient.id === searchIngredient.id;
+    });
+  }
+
   hasEnoughIngredientsForRecipe(recipeData) {
     return recipeData.ingredients.every((recipeIngredient => {
-      let existingIngredient = this.ingredients.find(ingredient => {
-        return ingredient.id === recipeIngredient.id;
-      });
+      let existingIngredient = this.findIngredient(this.ingredients, recipeIngredient);
       if (existingIngredient) {
         return existingIngredient.amount >= recipeIngredient.quantity.amount;
       }
     }))
   }
 
+  createMissingIngredient(existingIngredient, recipeIngredient, missingIngredients, ingredientsData) {
+    if (existingIngredient) {
+      missingIngredients = this.createIngredientIfDearth(existingIngredient, recipeIngredient, missingIngredients)
+    } else {
+      let nonExistingIngredient = this.findIngredient(ingredientsData, recipeIngredient);
+      nonExistingIngredient.amountNeeded = recipeIngredient.quantity.amount;
+      missingIngredients.push(nonExistingIngredient);
+    }
+    return missingIngredients;
+  }
+
+  createIngredientIfDearth(existingIngredient, recipeIngredient, missingIngredients) {
+    if (existingIngredient.amount < recipeIngredient.quantity.amount) {
+      missingIngredients.push({
+        id: existingIngredient.id,
+        name: existingIngredient.name,
+        amountNeeded: recipeIngredient.quantity.amount - existingIngredient.amount
+      })
+    }
+    return missingIngredients;
+  }
+
   ingredientsNeededForARecipe(recipeData, ingredientsData) {
     return recipeData.ingredients.reduce((missingIngredients, recipeIngredient) => {
-      let existingIngredient = this.ingredients.find(ingredient => {
-        return ingredient.id === recipeIngredient.id;
-      });
-      if (existingIngredient) {
-        if (existingIngredient.amount < recipeIngredient.quantity.amount) {
-          missingIngredients.push({
-            id: existingIngredient.id,
-            name: existingIngredient.name,
-            amountNeeded: recipeIngredient.quantity.amount - existingIngredient.amount
-          })
-        }
-      } else {
-        let nonExistingIngredient = ingredientsData.find(ingredient => {
-          return ingredient.id === recipeIngredient.id;
-        });
-        nonExistingIngredient.amountNeeded = recipeIngredient.quantity.amount;
-        missingIngredients.push(nonExistingIngredient);
-      }
+      let existingIngredient = this.findIngredient(this.ingredients, recipeIngredient);
+      missingIngredients = this.createMissingIngredient(existingIngredient, recipeIngredient, missingIngredients, ingredientsData);
       return missingIngredients;
     }, []);
   }
