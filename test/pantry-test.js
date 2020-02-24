f766155666a5aa35d216c15318d5ede137cb9145
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-24 14:11:16 -0700

    Add test pantry method that finds the amounts of ingredients needed to complete a recipe

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -1,230 +1,107 @@
 const chai = require('chai');
+
 const expect = chai.expect;
+import pantryData from './data/pantry-data';
+import recipeData from './data/recipe-data';
+import ingredientsData from './data/ingredient-data';
 
 const Pantry = require('../src/pantry');
 
 describe('Pantry', function() {
   let pantry;
-  let pantryData;
-  let recipeData;
+  let allIngredients;
+  let incompleteRecipe;
 
   beforeEach(function() {
-    pantryData = [
-                {
-                  "id": 11477,
-                  "amount": 4,
-                  "name": "zucchini squash",
-                  "estimatedCostInCents": 742
-                },
-                {
-                  "id": 11297,
-                  "amount": 4,
-                  "name": "flat leaf parsley leaves",
-                  "estimatedCostInCents": 1030
-                },
-                {
-                  "id": 1082047,
-                  "amount": 10,
-                  "name": "kosher salt",
-                  "estimatedCostInCents": 972
-                },
-                {
-                  "id": 20081,
-                  "amount": 5,
-                  "name": "wheat flour",
-                  "estimatedCostInCents": 142
-                },
-                {
-                  "id": 11215,
-                  "amount": 5,
-                  "name": "wheat flour",
-                  "estimatedCostInCents": 142
-                },
-                {
-                  "id": 2047,
-                  "amount": 6,
-                  "name": "salt",
-                  "estimatedCostInCents": 280
-                },
-                {
-                  "id": 1123,
-                  "amount": 8,
-                  "name": "eggs",
-                  "estimatedCostInCents": 472
-                },
-                {
-                  "id": 11282,
-                  "amount": 4,
-                  "name": "onions",
-                  "estimatedCostInCents": 439
-                },
-                {
-                  "id": 6172,
-                  "amount": 2,
-                  "name": "chicken stock",
-                  "estimatedCostInCents": 454
-                },
-                {
-                  "id": 2044,
-                  "amount": 2,
-                  "name": "basil",
-                  "estimatedCostInCents": 203
-                },
-                {
-                  "id": 2050,
-                  "amount": 4,
-                  "name": "vanilla",
-                  "estimatedCostInCents": 926
-                },
-                {
-                  "id": 1032009,
-                  "amount": 3,
-                  "name": "dried red chili",
-                  "estimatedCostInCents": 1015
-                },
-                {
-                  "id": 5114,
-                  "amount": 3,
-                  "name": "roasted chicken",
-                  "estimatedCostInCents": 708
-                },
-                {
-                  "id": 1017,
-                  "amount": 2,
-                  "name": "cream cheese",
-                  "estimatedCostInCents": 955
-                },
-                {
-                  "id": 19334,
-                  "amount": 7,
-                  "name": "bicarbonate of soda",
-                  "estimatedCostInCents": 582
-                },
-                {
-                  "id": 1001,
-                  "amount": 6,
-                  "name": "butter",
-                  "estimatedCostInCents": 618
-                },
-                {
-                  "id": 1145,
-                  "amount": 2,
-                  "name": "canned chipotle chilies in adobo",
-                  "estimatedCostInCents": 299
-                },
-                {
-                  "id": 10019903,
-                  "amount": 2,
-                  "name": "buttermilk",
-                  "estimatedCostInCents": 773
-                },
-                {
-                  "id": 9152,
-                  "amount": 4,
-                  "name": "lemon juice",
-                  "estimatedCostInCents": 274
-                },
-                {
-                  "id": 10611282,
-                  "amount": 3,
-                  "name": "white onions",
-                  "estimatedCostInCents": 449
-                },
-                {
-                  "id": 93607,
-                  "amount": 2,
-                  "name": "almondmilk",
-                  "estimatedCostInCents": 787
-                },
-                {
-                  "id": 14106,
-                  "amount": 4,
-                  "name": "white wine",
-                  "estimatedCostInCents": 675
-                },
-            ];
-    recipeData = {
-          "id": 595736,
-          "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-          "ingredients": [
-              {
-                  "id": 20081,
-                  "quantity": {
-                      "amount": 1.5,
-                      "unit": "c"
-                  }
-              },
-              {
-                  "id": 14106,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "tsp"
-                  }
-              },
-              {
-                  "id": 1123,
-                  "quantity": {
-                      "amount": 1,
-                      "unit": "large"
-                  }
-              },
-              {
-                  "id": 93607,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "c"
-                  }
-              },
-              {
-                  "id": 10611282,
-                  "quantity": {
-                      "amount": 3,
-                      "unit": "Tbsp"
-                  }
-              },
-              {
-                  "id": 19334,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "c"
-                  }
-              },
-              {
-                  "id": 2047,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "tsp"
-                  }
-              },
-              {
-                  "id": 9152,
-                  "quantity": {
-                      "amount": 4,
-                      "unit": "servings"
-                  }
-              },
-              {
-                  "id": 10019903,
-                  "quantity": {
-                      "amount": 2,
-                      "unit": "c"
-                  }
-              },
-              {
-                  "id": 1145,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "c"
-                  }
-              },
-              {
-                  "id": 2050,
-                  "quantity": {
-                      "amount": 0.5,
-                      "unit": "tsp"
-                  }
-              }]
-            }
+      incompleteRecipe = {
+        "id": 595736,
+        "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+        "ingredients": [
+            {
+                "id": 20081,
+                "quantity": {
+                    "amount": 11.5,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 14106,
+                "quantity": {
+                    "amount": 7.5,
+                    "unit": "tsp"
+                }
+            },
+            {
+                "id": 1123,
+                "quantity": {
+                    "amount": 1,
+                    "unit": "large"
+                }
+            },
+            {
+                "id": 93607,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 10611282,
+                "quantity": {
+                    "amount": 3,
+                    "unit": "Tbsp"
+                }
+            },
+            {
+                "id": 19334,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 2047,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "tsp"
+                }
+            },
+            {
+                "id": 9152,
+                "quantity": {
+                    "amount": 4,
+                    "unit": "servings"
+                }
+            },
+            {
+                "id": 10019903,
+                "quantity": {
+                    "amount": 2,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 1145,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 1,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "c"
+                }
+            },
+            {
+                "id": 2050,
+                "quantity": {
+                    "amount": 0.5,
+                    "unit": "tsp"
+                }
+            }]
+          }
     pantry = new Pantry(pantryData);
   })
 
@@ -238,5 +115,25 @@ describe('Pantry', function() {
 
   it('should determine whether my pantry has enough ingredients to cook a given meal', function() {
     expect(pantry.hasEnoughIngredientsForRecipe(recipeData)).to.eq(true);
-  })
+  });
+
+  it('should determine the amount of ingredients still needed to cook a given meal, based on whats in my pantry', function() {
+    expect(pantry.ingredientsNeededForARecipe(incompleteRecipe, ingredientsData)).to.deep.eq( [{
+        "id": 20081,
+        "name": "wheat flour",
+        "amountNeeded": 6.5,
+      },
+      {
+        "id": 14106,
+        "name": "white wine",
+        "amountNeeded": 3.5,
+      },
+      {
+        "id": 1,
+        "name": "vegetable stock",
+        "estimatedCostInCents": 613,
+        "amountNeeded": 0.5
+      }
+    ]);
+  });
 });
