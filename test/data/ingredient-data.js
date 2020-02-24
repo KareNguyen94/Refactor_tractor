35369a33fa32dc9942730c80c2bd520f4db6a22f
Jordan Whitten <jordanwa1947@gmail.com>
2020-02-23 17:07:49 -0700

    Add truncated test data to pass tests

---
+++ test/data/ingredient-data.js
@@ -0,0 +1,134 @@
+let ingredientsData = [
+  {
+    "id": 11477,
+    "name": "zucchini squash",
+    "estimatedCostInCents": 742
+  },
+  {
+    "id": 11297,
+    "name": "flat leaf parsley leaves",
+    "estimatedCostInCents": 1030
+  },
+  {
+    "id": 1082047,
+    "name": "kosher salt",
+    "estimatedCostInCents": 972
+  },
+  {
+    "id": 20081,
+    "name": "wheat flour",
+    "estimatedCostInCents": 142
+  },
+  {
+    "id": 11215,
+    "name": "wheat flour",
+    "estimatedCostInCents": 142
+  },
+  {
+    "id": 2047,
+    "name": "salt",
+    "estimatedCostInCents": 280
+  },
+  {
+    "id": 1123,
+    "name": "eggs",
+    "estimatedCostInCents": 472
+  },
+  {
+    "id": 11282,
+    "name": "onions",
+    "estimatedCostInCents": 439
+  },
+  {
+    "id": 6172,
+    "name": "chicken stock",
+    "estimatedCostInCents": 454
+  },
+  {
+    "id": 2044,
+    "name": "basil",
+    "estimatedCostInCents": 203
+  },
+  {
+    "id": 2050,
+    "name": "vanilla",
+    "estimatedCostInCents": 926
+  },
+  {
+    "id": 1032009,
+    "name": "dried red chili",
+    "estimatedCostInCents": 1015
+  },
+  {
+    "id": 5114,
+    "name": "roasted chicken",
+    "estimatedCostInCents": 708
+  },
+  {
+    "id": 1017,
+    "name": "cream cheese",
+    "estimatedCostInCents": 955
+  },
+  {
+    "id": 19334,
+    "name": "bicarbonate of soda",
+    "estimatedCostInCents": 582
+  },
+  {
+    "id": 1001,
+    "name": "butter",
+    "estimatedCostInCents": 618
+  },
+  {
+    "id": 1145,
+    "name": "canned chipotle chilies in adobo",
+    "estimatedCostInCents": 299
+  },
+  {
+    "id": 10019903,
+    "name": "buttermilk",
+    "estimatedCostInCents": 773
+  },
+  {
+    "id": 9152,
+    "name": "lemon juice",
+    "estimatedCostInCents": 274
+  },
+  {
+    "id": 10611282,
+    "name": "white onions",
+    "estimatedCostInCents": 449
+  },
+  {
+    "id": 93607,
+    "name": "almondmilk",
+    "estimatedCostInCents": 787
+  },
+  {
+    "id": 14106,
+    "name": "white wine",
+    "estimatedCostInCents": 675
+  },
+  {
+    "id": 11935,
+    "name": "catsup",
+    "estimatedCostInCents": 666
+  },
+  {
+    "id": 12151,
+    "name": "pistachio",
+    "estimatedCostInCents": 813
+  },
+  {
+    "id": 11821,
+    "name": "red sweet peppers",
+    "estimatedCostInCents": 1027
+  },
+  {
+    "id": 6615,
+    "name": "vegetable stock",
+    "estimatedCostInCents": 613
+  }
+]
+
+export default ingredientsData;
---
+++ test/data/pantry-data.js
@@ -0,0 +1,136 @@
+let pantryData = [
+            {
+              "id": 11477,
+              "amount": 4,
+              "name": "zucchini squash",
+              "estimatedCostInCents": 742
+            },
+            {
+              "id": 11297,
+              "amount": 4,
+              "name": "flat leaf parsley leaves",
+              "estimatedCostInCents": 1030
+            },
+            {
+              "id": 1082047,
+              "amount": 10,
+              "name": "kosher salt",
+              "estimatedCostInCents": 972
+            },
+            {
+              "id": 20081,
+              "amount": 5,
+              "name": "wheat flour",
+              "estimatedCostInCents": 142
+            },
+            {
+              "id": 11215,
+              "amount": 5,
+              "name": "wheat flour",
+              "estimatedCostInCents": 142
+            },
+            {
+              "id": 2047,
+              "amount": 6,
+              "name": "salt",
+              "estimatedCostInCents": 280
+            },
+            {
+              "id": 1123,
+              "amount": 8,
+              "name": "eggs",
+              "estimatedCostInCents": 472
+            },
+            {
+              "id": 11282,
+              "amount": 4,
+              "name": "onions",
+              "estimatedCostInCents": 439
+            },
+            {
+              "id": 6172,
+              "amount": 2,
+              "name": "chicken stock",
+              "estimatedCostInCents": 454
+            },
+            {
+              "id": 2044,
+              "amount": 2,
+              "name": "basil",
+              "estimatedCostInCents": 203
+            },
+            {
+              "id": 2050,
+              "amount": 4,
+              "name": "vanilla",
+              "estimatedCostInCents": 926
+            },
+            {
+              "id": 1032009,
+              "amount": 3,
+              "name": "dried red chili",
+              "estimatedCostInCents": 1015
+            },
+            {
+              "id": 5114,
+              "amount": 3,
+              "name": "roasted chicken",
+              "estimatedCostInCents": 708
+            },
+            {
+              "id": 1017,
+              "amount": 2,
+              "name": "cream cheese",
+              "estimatedCostInCents": 955
+            },
+            {
+              "id": 19334,
+              "amount": 7,
+              "name": "bicarbonate of soda",
+              "estimatedCostInCents": 582
+            },
+            {
+              "id": 1001,
+              "amount": 6,
+              "name": "butter",
+              "estimatedCostInCents": 618
+            },
+            {
+              "id": 1145,
+              "amount": 2,
+              "name": "canned chipotle chilies in adobo",
+              "estimatedCostInCents": 299
+            },
+            {
+              "id": 10019903,
+              "amount": 2,
+              "name": "buttermilk",
+              "estimatedCostInCents": 773
+            },
+            {
+              "id": 9152,
+              "amount": 4,
+              "name": "lemon juice",
+              "estimatedCostInCents": 274
+            },
+            {
+              "id": 10611282,
+              "amount": 3,
+              "name": "white onions",
+              "estimatedCostInCents": 449
+            },
+            {
+              "id": 93607,
+              "amount": 2,
+              "name": "almondmilk",
+              "estimatedCostInCents": 787
+            },
+            {
+              "id": 14106,
+              "amount": 4,
+              "name": "white wine",
+              "estimatedCostInCents": 675
+            },
+        ];
+
+  export default pantryData;
